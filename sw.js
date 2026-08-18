0c0b10a903282126f3c879e0375f542ebdad23f8
Dang Phong <Phongdq@sphinxjsc.com>
2026-08-18 21:10:16 +0700

    feat: PWA - manifest, service worker, icons, meta tags

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'tvtt-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './pages/add-word.html',
+  './pages/vocabulary.html',
+  './pages/review.html',
+  './pages/writing.html',
+  './css/style.css',
+  './js/config.js',
+  './js/utils.js',
+  './js/api.js',
+  './js/reviewService.js',
+  './js/review.js',
+  './js/writing.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './manifest.json',
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) { return key !== CACHE_NAME; })
+            .map(function(key) { return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request)
+      .then(function(res) {
+        var clone = res.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(e.request, clone);
+        });
+        return res;
+      })
+      .catch(function() {
+        return caches.match(e.request);
+      })
+  );
+});
